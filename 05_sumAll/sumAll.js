8bec01b650815e22fd7cb86969cee2328987ecae
Azeez Yusuf Olamilekan <highshow74@gmail.com>
2022-07-13 16:33:24 +0100

    Added: Solution to Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+      if (!Number.isInteger(value1) || !Number.isInteger(value2)) return "ERROR";
+  if (value1 < 0 || value2 < 0) return "ERROR";
+  if (value1 > value2) {
+    const temp = value1;
+    value1 = value2;
+    value2 = temp;
+  }
+
+  let sum = 0;
+  for (let i = value1; i < value2 + 1; i++) {
+    sum += i;
+  }
+  return sum;
+
+
 
 };
 
