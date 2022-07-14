488b62d325208bd8d11e5f75b1eefbbf342396e0
Azeez Yusuf Olamilekan <highshow74@gmail.com>
2022-07-14 12:14:47 +0100

    Solved Exercise O7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,27 @@
-const ftoc = function() {
+// F = C * (9/5) + 32
+// C = (F - 32) * (5/9)
 
+const ftoc = function(fahValue) {
+const celFraction = 5/9;
+const result = (fahValue - 32) * celFraction;
+const resultTo1DecPlace =  result.toFixed(1);
+return Number(resultTo1DecPlace)
 };
 
-const ctof = function() {
-
+const ctof = function(celsiusValue) {
+const fahFraction = 9/5;
+const result = (celsiusValue * fahFraction) + 32;
+const resultTo1DecPlace =  result.toFixed(1);
+return Number(resultTo1DecPlace)
 };
+
 
 // Do not edit below this line
 module.exports = {
   ftoc,
   ctof
 };
+
+
+
+
