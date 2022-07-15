668a013f3da86813dfcef5b7424269b79ea87037
Azeez Yusuf Olamilekan <highshow74@gmail.com>
2022-07-15 11:22:24 +0100

    Added: Solution to Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    stringToLowerCase = string.toLowerCase().replace(/[^a-z]/g, "");
+    const reverseString = stringToLowerCase.split('').reverse().join('');
+    if (reverseString == stringToLowerCase) {return true}
+    else {return false}
 };
 
 // Do not edit below this line
