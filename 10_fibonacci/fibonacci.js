2f3477f8155c12fe7449cdbf26fd32ef120f514e
Azeez Yusuf Olamilekan <highshow74@gmail.com>
2022-07-16 17:30:29 +0100

    Solved Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(numb) {
+  if (numb < 0) return "OOPS";
+  if (numb === 0) return 0;
+  let a = 0;
+  let b = 1;
+  for (let i = 1; i < numb; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return b;
 
 };
 
