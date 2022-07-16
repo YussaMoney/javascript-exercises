1b73b10e53e0426978eeefa1c302f9e8a766752a
Azeez Yusuf Olamilekan <highshow74@gmail.com>
2022-07-16 17:41:39 +0100

    Added: More Solution to Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,4 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
     return array.reduce((oldest, currentPerson) => {
         const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
         const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
